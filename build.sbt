6722690be0bebacace1c0e6caccf093b15422524
aosinaike <abiodun@tractionapps.co>
2021-09-17 11:57:33 +0100

    htpp4s implementation using cats effect

--- build.sbt
+++ build.sbt
@@ -4,11 +4,36 @@ version := "0.1"
 
 scalaVersion := "2.13.6"
 
+val Http4sVersion = "1.0.0-M21"//"0.23.3"
+
 libraryDependencies ++= Seq(
   "dev.zio" %% "zio" % "2.0.0-M2",
   "dev.zio" %% "zio-test" % "2.0.0-M2" % Test,
+  "org.typelevel" %% "cats-core" % "2.6.1",
+
   "org.postgresql" % "postgresql" % "42.2.23",
   "com.typesafe.slick" %% "slick" % "3.3.3",
   "com.typesafe.slick" %% "slick-hikaricp" % "3.3.3",
-  "org.slf4j" % "slf4j-nop" % "1.7.32"
+  "org.slf4j" % "slf4j-nop" % "1.7.32",
+
+  "net.liftweb" %% "lift-json" % "3.4.3",
+
+  "com.typesafe.akka" %% "akka-actor" % "2.6.16",
+  "com.typesafe.akka" %% "akka-slf4j" % "2.6.16",
+  "ch.qos.logback" % "logback-classic" % "1.2.5",
+
+  "io.circe" %% "circe-generic" % "0.14.1",
+  "io.circe" %% "circe-core" % "0.14.1",
+  "io.circe" %% "circe-parser" % "0.14.1",
+  "com.twitter" %% "twitter-server" % "21.8.0",
+
+  "org.http4s" %% "http4s-blaze-client" % Http4sVersion,
+  "org.http4s" %% "http4s-blaze-server" % Http4sVersion,
+  "org.http4s" %% "http4s-dsl"          % Http4sVersion,
+  "org.http4s" %% "http4s-circe"        % Http4sVersion,
 )
+
+//resolvers ++= Seq(
+//  "Spray repository" at "http://repo.spray.io",
+//  "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+//)
---
+++ src/main/scala/implementations/http4s/IMDB.scala
@@ -0,0 +1,150 @@
+package implementations.http4s
+
+import cats._
+import cats.effect._
+import cats.implicits._
+import org.http4s.circe._
+import org.http4s._
+import io.circe.generic.auto._
+import io.circe.syntax._
+import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
+import org.http4s.dsl._
+import org.http4s.dsl.impl._
+import org.http4s.headers._
+import org.http4s.implicits._
+import org.http4s.server._
+import org.http4s.server.blaze.BlazeServerBuilder
+
+import java.time.Year
+import java.util.UUID
+import scala.collection.mutable
+import scala.util.Try
+
+object IMDB extends IOApp {
+
+  //Domain
+  type Actor = String
+  case class Movie(id: String, title: String, year: Int, actors: List[Actor], director: String)
+  case class Director(firstName: String, lastName: String){
+    override def toString: Actor = s"$firstName $lastName"
+  }
+  case class DirectorDetails(firstName: String, lastName: String, genre: String)
+
+  //Internal database
+  val snjl: Movie = Movie(
+    "6bcbca1e-efd3-411d-9f7c-14b872444fce",
+    "Zack Snyder's Justice League",
+    2021,
+    List("Henry Cavill", "Gal Godot", "Ezra Miller", "Ben Affleck", "Ray Fisher", "Jason Momoa"),
+    "Zack Snyder"
+  )
+
+  val movies: Map[String, Movie] = Map(snjl.id -> snjl)
+
+  //business logic
+  private def findMovieById(movieId: UUID) =
+    movies.get(movieId.toString)
+
+  private def findMoviesByDirector(director: String): List[Movie] =
+    movies.values.filter(_.director == director).toList
+  /*
+  -GET All movies by director
+  -GET All actors for a movie
+   */
+
+  /*
+  GET movies?director=zack%20Synder&year=2021
+   */
+
+ //scala 3 use given
+//  given val yearQueryParamDecoder: QueryParamDecoder[Year] =
+//    QueryParamDecoder[Int].map(Year.of)
+
+  implicit val yearQueryParamDecoder: QueryParamDecoder[Year] =
+    QueryParamDecoder[Int].emap { y =>
+      Try(Year.of(y))
+        .toEither
+        .leftMap { tr =>
+          ParseFailure(tr.getMessage, tr.getMessage)
+        }
+    }
+  object DirectorQueryParamMatcher extends QueryParamDecoderMatcher[String]("director")
+  object YearQueryParamMatcher extends OptionalValidatingQueryParamDecoderMatcher[Year]("year")
+
+  def movieRoutes[F[_]: Monad]: HttpRoutes[F] = {
+    val dsl = Http4sDsl[F]
+    import dsl._
+
+    HttpRoutes.of[F] {
+      case GET -> Root / "movies" :? DirectorQueryParamMatcher(director) +& YearQueryParamMatcher(maybeYear) =>
+        val moviesByDirector = findMoviesByDirector(director)
+        maybeYear match {
+          case Some(y) =>
+            y.fold(
+              _ => BadRequest("The given year is not valid"),
+              year => {
+
+                val moviesByDirectorAndYear = moviesByDirector.filter(_.year == year.getValue())
+                Ok(moviesByDirectorAndYear.asJson)
+              }
+            )
+          case None => Ok(moviesByDirector.asJson)
+        }
+      case GET -> Root / "movies" / UUIDVar(movieId) / "actors" =>
+        findMovieById(movieId).map(_.actors) match {
+          case Some(actors) => Ok(actors.asJson)
+          case _ => NotFound(s"No movie with id $movieId found")
+        }
+    }
+  }
+
+  object DirectorVar {
+    def unapply(str: String): Option[Director] = {
+      if (str.nonEmpty && str.matches(".* .*")) {
+        Try {
+          val splitStr = str.split(' ')
+          Director(splitStr(0), splitStr(1))
+        }.toOption
+      } else None
+    }
+  }
+
+  val directorDetailsDB: mutable.Map[Director, DirectorDetails] =
+    mutable.Map(Director("Zack", "Zynder")-> DirectorDetails("Zack", "Snyder", "Action"))
+
+  def directorRoutes[F[_] : Monad]: HttpRoutes[F] = {
+    val dsl = Http4sDsl[F]
+    import dsl._
+    HttpRoutes.of[F] {
+      case GET -> Root / "directors" / DirectorVar(director) =>
+        directorDetailsDB.get(director) match {
+          case Some(dirDetails) => Ok(dirDetails.asJson)
+          case _ => NotFound(s"No director '$director' Found")
+        }
+    }
+  }
+
+  def allRoutes[F[_] : Monad]: HttpRoutes[F] = {
+    import cats.syntax.semigroupk._
+    movieRoutes[F] <+> directorRoutes[F]
+  }
+
+  def allRoutesComplete[F[_] : Monad]: HttpApp[F] = {
+    allRoutes[F].orNotFound
+  }
+
+  override def run(args: List[String]): IO[ExitCode] = {
+
+    val apis = Router(
+      "/api" -> IMDB.movieRoutes[IO],
+      "/api/private" -> IMDB.directorRoutes[IO]
+    ).orNotFound
+
+    BlazeServerBuilder[IO](runtime.compute)
+      .bindHttp(8089, "localhost")
+      .withHttpApp(allRoutesComplete)//.withHttpApp(apis)
+      .resource
+      .use(_ => IO.never)
+      .as(ExitCode.Success)
+  }
+}
